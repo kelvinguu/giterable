62eef1d2be9e4f43791f9b134fcde0f2c5f9a35f
Kelvin Gu <gu.kelvin@gmail.com>
2013-12-09 21:12:33 -0800

    added demo folder

---
+++ demo/Demo.java
@@ -0,0 +1,31 @@
+import com.github.giterable.Giterable;
+import com.github.giterable.GitLoader;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Demo {
+    public static void main(String[] args) throws IOException {
+
+        File repoPath = new File("demo_project/.git");
+        String revisionStr = "master";
+        // String revisionStr = "HEAD";
+        // String revisionStr = "aec1f2040b0eeef41f0f37535f8b4c6334b2a610";
+        // String revisionStr = "HEAD~1";
+
+        System.out.println("Loading repository at:");
+        System.out.println(repoPath.getCanonicalPath() + '\n');
+
+        Giterable gitFiles = new Giterable(repoPath, revisionStr);
+        GitLoader loader = new GitLoader(repoPath);
+
+        for (File file : gitFiles) {
+            String text = loader.getText(file, revisionStr);
+            byte[] bytes = loader.getBytes(file, revisionStr);
+
+            System.out.println("- - - - - - - - - -");
+            System.out.println(file.getPath());
+            System.out.println(text + '\n');
+        }
+    }
+}
---
+++ demo/demo.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+javac -cp .:giterable.jar:jgit.jar Demo.java
+java -cp .:giterable.jar:jgit.jar Demo
